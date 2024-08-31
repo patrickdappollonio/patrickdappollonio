c70ef84654f11d7ad4e0f2aa3ed456ed7c70f12f
Patrick D'appollonio <930925+patrickdappollonio@users.noreply.github.com>
2024-08-30 21:55:55 -0400

    Add support for data files from YAML (#4)

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"text/template"
 
 	"github.com/Masterminds/sprig/v3"
 	"golang.org/x/sync/errgroup"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
@@ -19,21 +21,65 @@ func main() {
 }
 
 var (
-	username     = envdefault("GITHUB_USERNAME", "patrickdappollonio")
-	rssfeed      = envdefault("RSS_FEED", "https://www.patrickdap.com/index.xml")
-	templateFile = envdefault("TEMPLATE_FILE", "template.md.gotmpl")
-	maxPRs       = envintdefault("MAX_PULL_REQUESTS", 10)
-	maxStarred   = envintdefault("MAX_STARRED_REPOS", 20)
-	maxArticles  = envintdefault("MAX_ARTICLES", 5)
-	disableRSS   = envbooldefault("DISABLE_RSS", false)
-	disablePRs   = envbooldefault("DISABLE_PULL_REQUESTS", false)
-	disableStars = envbooldefault("DISABLE_STARRED_REPOS", false)
+	username         = envdefault("GITHUB_USERNAME", "patrickdappollonio")
+	rssfeed          = envdefault("RSS_FEED", "https://www.patrickdap.com/index.xml")
+	templateFile     = envdefault("TEMPLATE_FILE", "template.md.gotmpl")
+	maxPRs           = envintdefault("MAX_PULL_REQUESTS", 10)
+	maxStarred       = envintdefault("MAX_STARRED_REPOS", 20)
+	maxArticles      = envintdefault("MAX_ARTICLES", 5)
+	disableRSS       = envbooldefault("DISABLE_RSS", false)
+	disablePRs       = envbooldefault("DISABLE_PULL_REQUESTS", false)
+	disableStars     = envbooldefault("DISABLE_STARRED_REPOS", false)
+	disableDataFiles = envbooldefault("DISABLE_DATA_FILES", false)
 )
 
 func run() error {
-	data, err := os.ReadFile(templateFile)
+	// Read the template file
+	tplFile, err := os.ReadFile(templateFile)
 	if err != nil {
+		if os.IsNotExist(err) {
+			return fmt.Errorf("template file %q does not exist", templateFile)
+		}
+
 		return fmt.Errorf("failed to read template file %q: %w", templateFile, err)
+	}
+
+	// Data files are YAML files in the current working directory (excluding
+	// subdirectories). The data is loaded into the template as a map of string
+	// to any. Data values are available under ".Data" and the file name without
+	// extension is used as the key.
+	additionalData := make(map[string]any)
+	if !disableDataFiles {
+		dir, err := os.ReadDir(".")
+		if err != nil {
+			return fmt.Errorf("failed to read directory: %w", err)
+		}
+
+		for _, entry := range dir {
+			name := strings.TrimSuffix(entry.Name(), filepath.Ext(entry.Name()))
+			fname := entry.Name()
+			ext := filepath.Ext(entry.Name())
+
+			if (ext == ".yaml" || ext == ".yml") && !strings.HasPrefix(name, ".") {
+				var contents any
+				dataFile, err := os.ReadFile(fname)
+				if err != nil {
+					if os.IsNotExist(err) {
+						return fmt.Errorf("data file %q does not exist", fname)
+					}
+
+					return fmt.Errorf("failed to read data file %q: %w", fname, err)
+				}
+
+				if err := yaml.Unmarshal(dataFile, &contents); err != nil {
+					return fmt.Errorf("failed to decode YAML data file %q: %w", fname, err)
+				}
+
+				if contents != nil {
+					additionalData[name] = contents
+				}
+			}
+		}
 	}
 
 	funcs := sprig.FuncMap()
@@ -41,17 +87,19 @@ func run() error {
 		funcs[k] = v
 	}
 
-	tmpl, err := template.New("template").Funcs(fncs).Parse(string(data))
+	tmpl, err := template.New("template").Funcs(fncs).Parse(string(tplFile))
 	if err != nil {
 		return fmt.Errorf("failed to parse template: %w", err)
 	}
 
-	eg := errgroup.Group{}
+	var (
+		eg           = errgroup.Group{}
+		prs          []PullRequest
+		starredRepos []StarredRepo
+		articles     []Article
+	)
 
-	var prs []PullRequest
-	var starredRepos []StarredRepo
-	var articles []Article
-
+	// Fetch pull requests
 	eg.Go(func() error {
 		if disablePRs {
 			return nil
@@ -65,6 +113,7 @@ func run() error {
 		return nil
 	})
 
+	// Fetch starred repos
 	eg.Go(func() error {
 		if disableStars {
 			return nil
@@ -78,6 +127,7 @@ func run() error {
 		return nil
 	})
 
+	// Fetch RSS feed
 	eg.Go(func() error {
 		if disableRSS {
 			return nil
@@ -91,18 +141,24 @@ func run() error {
 		return nil
 	})
 
+	// Wait for all goroutines to finish
 	if err := eg.Wait(); err != nil {
 		return err
 	}
 
+	// Execute the template
 	if err := tmpl.Execute(os.Stdout, struct {
-		PullRequests []PullRequest
-		StarredRepos []StarredRepo
-		Articles     []Article
+		GitHubUsername string
+		PullRequests   []PullRequest
+		StarredRepos   []StarredRepo
+		Articles       []Article
+		Data           map[string]any
 	}{
-		PullRequests: prs,
-		StarredRepos: starredRepos,
-		Articles:     articles,
+		GitHubUsername: username,
+		PullRequests:   prs,
+		StarredRepos:   starredRepos,
+		Articles:       articles,
+		Data:           additionalData,
 	}); err != nil {
 		return fmt.Errorf("failed to execute template: %w", err)
 	}
